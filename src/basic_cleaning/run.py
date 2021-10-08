34f634282e45045fb6092dd6e70e2907fccaef44
Jinha Kim <kimjinhaha@gmail.com>
2021-10-08 16:06:20 +0200

    fix outliers for longitude/latitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,6 +27,10 @@ def go(args):
     # convert last_review to datetime
     df["last_review"] = pd.to_datetime(df["last_review"])
 
+    # filter outliers for longitude/latitude
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # save the cleaned dataset
     df.to_csv("clean_sample.csv", index=False)
 
